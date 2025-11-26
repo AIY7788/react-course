1501af5257c40a420a73be67d1e0e77a0ee3c177
AIY7788 <aiykhounnakhon@gmail.com>
2025-11-26 17:58:53 +0700

    Create page not found

---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import "./NotFoundPage.css";
+
+export function NotFoundPage() {
+  return (
+    <>
+      <title>Page Not Found</title>
+
+      <Header />
+
+      <div className="not-found-page">
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
+    </>
+  );
+}
