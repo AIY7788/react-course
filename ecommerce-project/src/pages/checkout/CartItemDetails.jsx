70bc418eab67b04b9c5e893bbfbcab37c42f188a
AIY7788 <aiykhounnakhon@gmail.com>
2025-12-05 12:53:08 +0700

    Implement quantity update functionality in CartItemDetails and style input field

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,40 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({ item, loadCart }) {
+  const [showTextbox, setShowTextbox] = useState(false);
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const updateQuantity = (e) => {
+    setQuantity(e.target.value);
+  };
+
+  const handleUpdateQuantity = async () => {
+    if (showTextbox) {
+      await axios.put(`api/cart-items/${item.productId}`, {
+        quantity: Number(quantity),
+      });
+      await loadCart();
+
+      setShowTextbox(false);
+    } else setShowTextbox(true);
+  };
+
+  const handleKeyDown = async (e) => {
+    if (e.key === "Enter") {
+      await axios.put(`api/cart-items/${item.productId}`, {
+        quantity: Number(quantity),
+      });
+      await loadCart();
+
+      setShowTextbox(false);
+    } else if (e.key === "Escape") {
+      setQuantity(item.quantity);
+      setShowTextbox(false);
+    }
+  };
+
   const handleDeleteItem = async () => {
     await axios.delete(`api/cart-items/${item.productId}`);
     await loadCart();
@@ -18,9 +51,28 @@ export function CartItemDetails({ item, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{item.quantity}</span>
+            Quantity:{" "}
+            <input
+              value={quantity}
+              onKeyDown={handleKeyDown}
+              onChange={updateQuantity}
+              className="update-quatity-input"
+              type="text"
+              style={{ display: showTextbox ? "inline-block" : "none" }}
+            />{" "}
+            <span
+              className="quantity-label"
+              style={{ display: showTextbox ? "none" : "inline-block" }}
+            >
+              {item.quantity}
+            </span>
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={handleUpdateQuantity}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={handleDeleteItem}
